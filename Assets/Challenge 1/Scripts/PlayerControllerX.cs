8e6daa17a967c071ad5e9c6f9abdf909db7f663c
Nguyen Duc Viet <ngducviet2105@gmail.com>
2023-04-18 15:13:53 +0700

    Add project files.

---
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    float speed = 15f;
+    float rotationSpeed = 25f;
+    float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Rotate(Vector3.right, verticalInput * Time.deltaTime * rotationSpeed);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 30f;
+    private float turnSpeed = 25f;
+    private float horizontalInput;
+    private float forwardInput;
+    public Camera mainCamera; 
+    public Camera hoodCamera;
+    public KeyCode switchKey;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // This is where we get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // We move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // We turn the vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+        if(Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
+    }
+}
